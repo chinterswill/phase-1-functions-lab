0a32d27bcd7dd1c86977640f543831499b543da2
Kamran Hussein <chinterswill@gmail.com>
2022-10-31 16:49:54 -0400

    commit tree 42f0529c73192199ce726c7c18cff5a29a7e9e91

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    if (someValue > 42) {
+        return (someValue - 42);
+    }
+    else if (someValue <= 42) {
+        return (42 - someValue);
+    }
+        
+    
+}
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+
+}
+
+function distanceTravelledInFeet(num1, num2) {
+    if (num1 > num2) {
+        return (num1 - num2) * 264;
+    }
+    else if (num2 > num1) {
+        return (num2 - num1) * 264;
+    }
+}
+
+function calculatesFarePrice(start,destination) {
+    let distance = distanceTravelledInFeet(start,destination);
+    if (distance <= 400){
+        return 0;
+    }
+    if (distance>=400 && distance<=2000){
+        return (distance-400)* 0.02;
+    }
+    else if(distance>=2000&& distance<=2500){
+        return 25;
+    }
+    else if (distance > 2500) {
+        return "cannot travel that far"
+    }
+}
